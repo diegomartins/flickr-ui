917530090272843d5faf0a82f3df2077b5ac0e2a
Diego Martins <diegomartins.br@gmail.com>
2017-09-13 23:00:15 +1000

    Improving the search bar

--- src/app/flickr/search-bar/search-bar.component.ts
+++ src/app/flickr/search-bar/search-bar.component.ts
@@ -1,10 +1,12 @@
 import { Component, EventEmitter, Output, AfterViewInit } from "@angular/core";
 import { FlickrApiService } from "app/shared/api/flickr-api.service";
 import { Subject } from "rxjs/Rx";
+import { TagSelectionService } from "app/shared/services/tag-selection.service";
 
 @Component({
     selector: "search-bar",
-    templateUrl: "./search-bar.component.html"
+    templateUrl: "./search-bar.component.html",
+    styleUrls: ["./search-bar.component.css"]
 })
 export class SearchBarComponent implements AfterViewInit {
 
@@ -14,13 +16,13 @@ export class SearchBarComponent implements AfterViewInit {
 
     private searchTermStream = new Subject<any>();
 
-    constructor(private flickrApiService: FlickrApiService) {}
+    constructor(private flickrApiService: FlickrApiService, private tagSelectionService: TagSelectionService) { }
 
     ngAfterViewInit(): void {
 
-        this.searchTermStream.debounceTime(300).distinctUntilChanged().subscribe(term => {
-            this.search(term);
-        });
+        this.subscribeToTagSelection();
+        this.subscribeToSearchTermChanges();
+        this.search("");
     }
 
     searchChanged(query) {
@@ -28,13 +30,33 @@ export class SearchBarComponent implements AfterViewInit {
     }
 
 
+    private subscribeToTagSelection() {
+
+        this.tagSelectionService.subscribe(data => {
+
+            if (data) {
+                this.searchTerm = data;
+                this.search(this.searchTerm);
+            }
+        });
+    }
+
+    private subscribeToSearchTermChanges() {
+
+         this.searchTermStream.debounceTime(300).distinctUntilChanged().subscribe(term => {
+            this.search(term);
+        });
+    }
+
     private search(query) {
 
-        this.flickrApiService.searchPictures(query).subscribe(data => {
+        const tags = query.split(" ").join(",");
+
+        this.flickrApiService.searchPictures(tags).subscribe(data => {
             this.onSearchResult.emit(data.items);
         }, error => {
-            console.error("Error");
-            //TODO: Handle error correctly
+            console.error(error);
+            //TODO: Handle error correctly (toaster notification, perhaps)
         });
     }
 }
