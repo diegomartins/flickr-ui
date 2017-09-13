e3f7af36613ee4b18ddef5fa4c7210783d7f16be
Diego Martins <diegomartins.br@gmail.com>
2017-09-13 22:59:28 +1000

    Improving the entry component. Adding placeholder for unloaded picture (have plans to add a loading animation for each picture). Improving tags on hover

--- src/app/flickr/entry/entry.component.ts
+++ src/app/flickr/entry/entry.component.ts
@@ -1,11 +1,30 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, Output, EventEmitter, OnInit } from "@angular/core";
+import { TagSelectionService } from "app/shared/services/tag-selection.service";
 
 @Component({
     selector: "entry",
     templateUrl: "./entry.component.html",
     styleUrls: ["./entry.component.css"]
 })
-export class EntryComponent {
+export class EntryComponent implements OnInit {
 
     @Input() entry: any;
+
+    loadingImage = false;
+
+    constructor(private tagSelectionService: TagSelectionService) { }
+
+    ngOnInit(): void {
+        console.log("ON INIT")
+        this.loadingImage = true;
+    }
+
+    imageLoaded() {
+        console.log("CALLED")
+        this.loadingImage = false;
+    }
+
+    handleTagClicked(tag) {
+        this.tagSelectionService.publish(tag);
+    }
 }
